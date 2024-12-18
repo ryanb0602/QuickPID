5ecbc87ea146568757cbebb01fdeb755ebaf6813
Ryan Berube <rberube@intelligentsensinglabs.com>
2024-12-18 18:13:46 -0500

    Removed Arduino.h reference

--- src/QuickPID.cpp
+++ src/QuickPID.cpp
@@ -4,11 +4,6 @@
    Based on the Arduino PID_v1 Library. Licensed under the MIT License.
  **********************************************************************************/
 
-#if ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
 
 #include "QuickPID.h"
 
